bff5da923a8284c1342b1db0f93b53a85e4b391d
xxxb-g <xxxbGamer@proton.me>
2025-06-10 22:28:13 +0200

    Uninstaller.sh kompakter gemacht

--- Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller.sh
+++ Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller.sh
@@ -1,7 +1,3 @@
-rm -f /usr/bin/TippX.py
-rm -f /usr/bin/TippX.sh
-rm -f /usr/bin/TippX
-rm -f /usr/share/applications/TippX.desktop
-rm -f /usr/share/icons/TippX-Logo.png
-rm -f /usr/share/man/man1/TippX.1.gz
+rm -f /usr/bin/TippX.py /usr/bin/TippX.sh /usr/bin/TippX /usr/share/applications/TippX.desktop
 rm -rf /usr/share/doc/TippX
+rm -f /usr/share/icons/TippX-Logo.png /usr/share/man/man1/TippX.1.gz
