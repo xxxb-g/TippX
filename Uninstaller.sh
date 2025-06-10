e049802bb2fecd148f112342880430f9ff2394ac
xxxb-g <xxxbGamer@proton.me>
2025-06-10 12:58:37 +0200

    Distributionsunabhängige Installierer hinzugefügt
    Distributionsunabhängige Installationsdateien wurden hinzugefügt.

---
+++ Uninstaller.sh
@@ -0,0 +1,7 @@
+rm -f /usr/bin/TippX.py
+rm -f /usr/bin/TippX.sh
+rm -f /usr/bin/TippX
+rm -f /usr/share/applications/TippX.desktop
+rm -f /usr/share/icons/TippX-Logo.png
+rm -f /usr/share/man/man1/TippX.1.gz
+rm -rf /usr/share/doc/TippX
