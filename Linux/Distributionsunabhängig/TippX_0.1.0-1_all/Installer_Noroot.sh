4573d7169b6db3fe2786264440e3d53f522066e7
xxxb-g <xxxbGamer@proton.me>
2025-06-27 14:21:12 +0200

    Syntaxfehler behoben

--- Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Installer_Noroot.sh
+++ Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Installer_Noroot.sh
@@ -1,16 +1,20 @@
-cp main.py $(HOME)/bin/TippX.py
-echo '#!/bin/bash\npython3 $(HOME)/bin/TippX.py' > $(HOME)/bin/TippX.sh
-chmod +x $(HOME)/bin/TippX.sh
-ln -f -s $(HOME)/bin/TippX.sh $(HOME)/bin/TippX
-echo '[Desktop Entry]\nVersion=1.0\nType=Application\nName=TippX\nComment=Deutsches 10-Finger-Schreibsystem lernen\nExec=TippX\nIcon=$(HOME)/.local/share/icons/TippX-Logo.png\nTerminal=true\nCategories=Utility;Application;\nStartupNotify=true' > $(HOME)/.local/share/applications/TippX.desktop
-mkdir -p $(HOME)/.local/share/doc/TippX
-cp ./LICENSE $(HOME)/.local/share/doc/TippX
-cp ./README.md $(HOME)/.local/share/doc/TippX
-echo 'Du hast die Version 0.1.0 kostenlos installiert. Du kann mich auf https://coindrop.to/TippX unterstützen.' > $(HOME)/.local/share/doc/TippX/Version
-cp ./Logo.png $(HOME)/.local/share/icons/TippX-Logo.png
-mkdir -p $(HOME)/.local/share/man/man1/
+git clone https://github.com/xxxb-g/TippX.git ./tmp
+cd ./tmp
+cp main.py ~/bin/TippX.py
+echo '#!/bin/bash\npython3 ~/bin/TippX.py' > ~/bin/TippX.sh
+chmod +x ~/bin/TippX.sh
+ln -f -s ~/bin/TippX.sh ~/bin/TippX
+echo '[Desktop Entry]\nVersion=1.0\nType=Application\nName=TippX\nComment=Deutsches 10-Finger-Schreibsystem lernen\nExec=TippX\nIcon=~/.local/share/icons/TippX-Logo.png\nTerminal=true\nCategories=Utility;Application;\nStartupNotify=true' > ~/.local/share/applications/TippX.desktop
+mkdir -p ~/.local/share/doc/TippX
+cp ./LICENSE ~/.local/share/doc/TippX
+cp ./README.md ~/.local/share/doc/TippX
+echo 'Du hast die Version 0.1.0 kostenlos installiert. Du kann mich auf https://coindrop.to/TippX unterstützen.' > ~/.local/share/doc/TippX/Version
+cp ./Logo.png ~/.local/share/icons/TippX-Logo.png
+mkdir -p ~/.local/share/man/man1/
 echo -e "(C) Copyright 2025 Benedikt Goldhahn <xxxbGamer@proton.me>\nTippX ist ein Programm zum lernen des 10-Finger-Schreibsystems. Zum Starten musst du nur TippX in deinem Startmenu suchen und öffnen. Alternativ kanst du den Befehl "TippX" im Terminal ausführen.\nAm Anfang wirst du gefragt, welches Level du trainieren willst. Gib die entsprechende Zahl (z.B. 1) ein. Danach wirst du gefragt, wie lange du trainieren willst. Gib auch hier eine Zahl ein. Nun beginnt das Training. Tippe den Satz, der auf dem Bildschirm angezeigt wird und bestätige mit der Enter-Taste. Nach Ablauf der Zeit wird dir eine Statistik angezeigt." > TippX.1
 gzip -f TippX.1
-cp TippX.1.gz $(HOME)/.local/share/man/man1/TippX.1.gz
+cp TippX.1.gz ~/.local/share/man/man1/TippX.1.gz
 rm -f TippX.1
 rm -f TippX.1.gz
+cd ..
+rm -rf ./tmp
--- Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller_Noroot.sh
+++ Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller_Noroot.sh
@@ -1,3 +1,3 @@
-rm -f $(HOME)/bin/TippX.py $(HOME)/bin/TippX.sh $(HOME)/bin/TippX $(HOME)/.local/share/applications/TippX.desktop
-rm -rf $(HOME)/.local/share/doc/TippX
-rm -f $(HOME)/.local/share/icons/TippX-Logo.png $(HOME)/.local/share/man/man1/TippX.1.gz
+rm -f ~/bin/TippX.py ~/bin/TippX.sh ~/bin/TippX ~/.local/share/applications/TippX.desktop
+rm -rf ~/.local/share/doc/TippX
+rm -f ~/.local/share/icons/TippX-Logo.png ~/.local/share/man/man1/TippX.1.gz
