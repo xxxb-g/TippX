9773d6e6a3b506b82719998be42bbf82ff8e9454
xxxb-g <xxxbGamer@proton.me>
2025-06-27 14:14:18 +0200

    Noroot zum (Un)Installer hinzugefügt
    Der (Un)Installer enthält nun eine Option, TippX ohne SU- Rechte (Superuser- bzw Root-Rechte) für den angemeldeten Nutzer zu (de)installieren.

---
+++ Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller_Noroot.sh
@@ -0,0 +1,3 @@
+rm -f $(HOME)/bin/TippX.py $(HOME)/bin/TippX.sh $(HOME)/bin/TippX $(HOME)/.local/share/applications/TippX.desktop
+rm -rf $(HOME)/.local/share/doc/TippX
+rm -f $(HOME)/.local/share/icons/TippX-Logo.png $(HOME)/.local/share/man/man1/TippX.1.gz
