fb42a8513c54fab3df24a9fff2b7f8b4238b10e5
xxxb-g <xxxbGamer@proton.me>
2025-11-23 13:51:31 +0100

    Update
    AppImage Unterstützung hinzugefügt, Struktur geändert.

---
+++ Linux/Distributionsunabhängig/Scripts/Uninstaller.sh
@@ -0,0 +1,3 @@
+rm -f /usr/bin/TippX.py /usr/bin/TippX.sh /usr/bin/TippX /usr/share/applications/TippX.desktop
+rm -rf /usr/share/doc/TippX
+rm -f /usr/share/icons/TippX-Logo.png /usr/share/man/man1/TippX.1.gz
--- Linux/Distributionsunabhängig/TippX_0.1.0-1_all/Uninstaller_Noroot.sh
+++
@@ -1,3 +0,0 @@
-rm -f ~/bin/TippX.py ~/bin/TippX.sh ~/bin/TippX ~/.local/share/applications/TippX.desktop
-rm -rf ~/.local/share/doc/TippX
-rm -f ~/.local/share/icons/TippX-Logo.png ~/.local/share/man/man1/TippX.1.gz
